7ab079a127bb6eb29778432b2c72e64d77d6aacc
Mushroom6228 <mushroom.g6228@gmail.com>
2025-12-29 13:52:34 -0300

    ajustes finais

--- mobile-menu.js
+++ mobile-menu.js
@@ -4,17 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
   const hamburgerMenu = document.getElementById("hamburgerMenu");
   const menuLinks = document.querySelectorAll(".menu-link");
   const headerContainer = document.querySelector(".header-container");
+  const body = document.querySelector("body");
 
   function openMenu() {
     hamburgerMenu.classList.add("active");
     hamburgerBtn.classList.add("active");
     headerContainer.classList.add("menu-open");
+    body.classList.add("no-scroll");
   }
 
   function closeMenu() {
     hamburgerMenu.classList.remove("active");
     hamburgerBtn.classList.remove("active");
     headerContainer.classList.remove("menu-open");
+    body.classList.remove("no-scroll");
   }
 
   if (hamburgerBtn) {
